6c8179294f880be27e2b61578da91c45e01d008e
ansango <anibalsantosgo@gmail.com>
2021-09-22 23:32:16 +0200

    reactivity

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,41 @@
 <script lang="ts">
-	export let name: string;
+  export let name: string;
+  export let img: string;
+  let count: number = 0;
+  const increment = () => {
+    count++;
+  };
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>Hello {name}!</h1>
+  <img src={img} alt={name} />
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
+  <button on:click={increment}>Increment +1</button>
+  <div>{count} times clicked</div>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import App from './App.svelte';
+import App from "./App.svelte";
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
+  target: document.body,
+  props: {
+    name: "world",
+    img: "https://picsum.photos/200/300",
+  },
 });
 
 export default app;

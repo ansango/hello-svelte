167d80740e5bf1fc8bd0755e67c1747c004890bc
ansango <anibalsantosgo@gmail.com>
2021-09-24 00:43:30 +0200

    movies list

--- src/Input.svelte
+++ src/Input.svelte
@@ -14,27 +14,44 @@
             response = data.Search || [];
             loading = false;
           });
-      }, 3000);
+      }, 1000);
     }
   }
 </script>
 
-<div>
+<div class="container">
   <input {value} on:input={handleInput} />
   {#if loading}
     <div>Loading...</div>
   {:else if response.length > 0}
-    <div>Tenemos {response.length} pelis</div>
+    <div class="film-container">
+      {#each response as movie}
+        <div class="film">
+          <h3>{movie.Title} - <span>{movie.Year}</span></h3>
+          <img src={movie.Poster} alt={movie.Title} />
+        </div>
+      {/each}
+    </div>
   {:else}<div>No tenemos pelis</div>
   {/if}
 </div>
 
 <style>
-  div {
+  .container {
     margin: auto;
-    max-width: 600px;
+    max-width: 1200px;
     display: flex;
     justify-content: center;
     flex-direction: column;
+  }
+
+  .film-container {
+    display: grid;
+    grid-template-columns: repeat(2, minmax(200px, 1fr));
+  }
+
+  .film {
+    text-align: center;
+    padding: 1rem;
   }
 </style>

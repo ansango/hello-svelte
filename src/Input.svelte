38eba126b88ebb64cf29db85cdb7f6a65f2d120c
ansango <anibalsantosgo@gmail.com>
2021-09-24 00:29:34 +0200

    api if else

--- src/Input.svelte
+++ src/Input.svelte
@@ -1,26 +1,40 @@
 <script lang="ts">
   let value = "";
+  let loading = false;
   let response = [];
   const handleInput = (event) => (value = event.target.value);
 
   $: {
     if (value.trim().length > 5) {
-      fetch(`https://www.omdbapi.com/?s=${value}&apikey=422350ff`)
-        .then((res) => res.json())
-        .then((data) => {
-          response = data.Search;
-        });
+      loading = true;
+      setTimeout(() => {
+        fetch(`https://www.omdbapi.com/?s=${value}&apikey=422350ff`)
+          .then((res) => res.json())
+          .then((data) => {
+            response = data.Search || [];
+            loading = false;
+          });
+      }, 3000);
     }
   }
 </script>
 
 <div>
   <input {value} on:input={handleInput} />
+  {#if loading}
+    <div>Loading...</div>
+  {:else if response.length > 0}
+    <div>Tenemos {response.length} pelis</div>
+  {:else}<div>No tenemos pelis</div>
+  {/if}
 </div>
 
 <style>
   div {
+    margin: auto;
+    max-width: 600px;
     display: flex;
     justify-content: center;
+    flex-direction: column;
   }
 </style>
